de4645ec0a2f8841f8681eaf0c9014f2069436de
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-05-23 20:48:45 +0200

    created common app

---
+++ common_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CommonAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'basic_dtl_project_bee_farm_website.common_app'
