4e843a150f1ee4d11ed5707f44d946f0155b0bae
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-05-24 19:22:44 +0200

    created product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,9 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
+
 # Create your models here.
+class Products(models.Model):
+    name = models.CharField(max_length=20, blank=False)
+    price = models.FloatField(blank=False, validators=[MinValueValidator(0), MaxValueValidator(500)])
+    image = models.ImageField(upload_to='images/')
--- urls.py
+++ urls.py
@@ -1,9 +1,11 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
 import basic_dtl_project_bee_farm_website.common_app.urls
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(basic_dtl_project_bee_farm_website.common_app.urls))
-]
+                  path('admin/', admin.site.urls),
+                  path('', include(basic_dtl_project_bee_farm_website.common_app.urls))
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
