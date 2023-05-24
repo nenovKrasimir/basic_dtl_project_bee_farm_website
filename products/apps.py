8197b49516d6eb180b4abd0f7b876cb580ef26cc
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-05-24 19:12:39 +0200

    created products_app

---
+++ products/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProductsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'basic_dtl_project_bee_farm_website.products'
