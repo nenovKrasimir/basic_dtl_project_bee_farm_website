d15bae92556b2a760207b524b3ce561b6e69ff5c
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-05-27 19:53:52 +0200

    added cart model

--- products/models.py
+++ products/models.py
@@ -2,12 +2,18 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
+class Cart(models.Model):
+    def __str__(self):
+        return f"Cart {self.products_set.all()}"
+
+
 # Create your models here.
 class Products(models.Model):
     name = models.CharField(max_length=20, blank=False)
     price = models.FloatField(blank=False, validators=[MinValueValidator(0), MaxValueValidator(500)])
     description = models.TextField(max_length=100)
     image = models.ImageField(upload_to='images/')
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
